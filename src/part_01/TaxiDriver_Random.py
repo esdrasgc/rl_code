650acc7b853d5729914a480a15055aadca1c4aac
Fabricio J Barth <fabricio.barth@gmail.com>
2023-04-19 14:53:31 -0300

    depois 1a aula RL na disciplina de IA

---
+++ src/part_01/TaxiDriver_Random.py
@@ -0,0 +1,16 @@
+import gymnasium as gym
+env = gym.make("Taxi-v3", render_mode='human').env
+
+done = False
+episodios = 1000
+
+for i in range(episodios):
+    state = env.reset()
+    while not done:
+        action = env.action_space.sample()
+        next_state, reward, done, truncated, info = env.step(action)
+        #
+        # state, action, reward, next_state p/ aprender uma policy
+        #
+        
+        #state.render()
